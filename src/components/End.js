e784b79ff41ccb78468bf84dac048b40e0addc4d
MohammedAliBaaqail <aamohammed@graduate.utm.my>
2022-09-01 22:05:16 +0800

    done

---
+++ src/components/End.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function End({ isCorrect, solution, turn }) {
+  return (
+    <div className="End">
+      {isCorrect && (
+        <div>
+          <h1>You Won!</h1>
+          <p className="solution">{solution} Is correct!</p>
+          <p>You found the solution in {turn} guesses :)</p>
+        </div>
+      )}
+      {!isCorrect && (
+        <div>
+          <h1>You Lost!</h1>
+          <h3>The correct answer is:</h3>
+          <p className="solution"> {solution}</p>
+          <p>Try again :) </p>
+        </div>
+      )}
+    </div>
+  )
+}
