1528024edd50a4084552fb54aa7f7f9dd8c2dac0
MohammedAliBaaqail <aamohammed@graduate.utm.my>
2022-09-02 19:58:43 +0800

    .

--- src/components/main.js
+++ src/components/main.js
@@ -18,7 +18,7 @@ export default function Main() {
     <div className={`${theme} main`} >
     <div className="switch">
         <label className={themeInvText}> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
-        <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        <ReactSwitch onChange={toggleTheme} checked={theme === "light"} />
       </div>
     <h1 className={themeInvText} >Real Fake Wordle </h1>
     <h2 className={themeInvText}>{type} , {definition}</h2>
